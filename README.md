d33e3b165183e823fab02ef5737aa5573125cf47
redroot <luke@red-root.com>
2017-12-23 01:11:03 +0000

    got fairly far but stumped here

--- README.md
+++ README.md
@@ -13,6 +13,11 @@ Attempts at solving a gogen puzzle solver in various languages
 
 #### Go
 
+<<<<<<< HEAD
 - Set isn't part of the standard library so used a great third party representation. I definitely missed the unified API that Ruby provides for array/sets and maps.
 - Typed definition especially involving maps took a little while to grok, plenty of the shortcuts in Ruby unavailable but I appreciated it, if it compiles it tended to run
 - Lack of `cond || default` was a little frustrating.
+=======
+- Set isn't part of the standard library so used a great third party representation
+- Typed definition especially involving maps took a little while to grok, plenty of the shortcuts in Ruby unavailable but I appreciated it, if it compiles it tended to run
+>>>>>>> got fairly far but stumped here
--- go/gogen.go
+++ go/gogen.go
@@ -6,11 +6,22 @@ import (
   "path/filepath"
   "os"
   "strings"
+  "gopkg.in/fatih/set.v0"
 )
 
 const textInputSplit = "#####"
 const gridSize = 5
 const blankCharacter = "_"
+const availableLetters = "ABCDEFGHIJKLMNOPQRSTUVWXY"
+
+type Pos struct {
+  x int
+  y int
+}
+
+type Grid [gridSize][gridSize]string
+type WordList []string
+type LetterPosSet map[string]*set.Set
 
 func log(msg string) {
   fmt.Printf("[GOGEN] %s\n", msg)
@@ -22,27 +33,81 @@ func checkErr(e error) {
   }
 }
 
-func textInputToGrid(textInput string) ([5][5]string) {
+func getLetterPosSetKeys(input LetterPosSet) (*set.Set) {
+  keys := set.New()
+  for k := range(input) {
+    keys.Add(k)
+  }
+  return keys
+}
+
+func textInputToGrid(textInput string) (Grid, LetterPosSet) {
   newGrid := [gridSize][gridSize]string{}
+  lettersFound := make(LetterPosSet)
   lines := strings.Split(strings.Trim(textInput, "\n"), "\n")
   for row := range(lines) {
     cells := strings.Split(lines[row], " ")
     for col, val := range(cells) {
       newGrid[col][row] = val
+      posSet := set.New(Pos{col, row})
+      lettersFound[val] = posSet
     }
   }
-  return newGrid
+  return newGrid, lettersFound
 }
 
-func extractDataFromFile(puzzle string) ([5][5]string, []string) {
+func extractDataFromFile(puzzle string) (Grid, LetterPosSet, WordList) {
   absPath, err := filepath.Abs(fmt.Sprintf("../examples/%s-unsolved.txt", puzzle))
   checkErr(err)
   data, err := ioutil.ReadFile(absPath)
   checkErr(err)
   parts := strings.Split(string(data), textInputSplit)
-  grid := textInputToGrid(parts[0])
+  grid, lettersFound := textInputToGrid(parts[0])
   words := strings.Split(parts[1], "\n")
-  return grid, words
+  return grid, lettersFound, words
+}
+
+func buildAllPositions() (*set.Set) {
+  s := set.New()
+  for row := 0; row < gridSize; row++ {
+    for col := 0; col < gridSize; col++ {
+      s.Add(Pos{col, row})
+    }
+  }
+  return s
+}
+
+func buildLettersToFind(lettersFound LetterPosSet) (LetterPosSet) {
+  allPositions := buildAllPositions()
+  allLetters := strings.Split(availableLetters, "")
+  allLettersSet := set.New()
+  for _, l := range(allLetters) {
+    allLettersSet.Add(l)
+  }
+
+  knownLetters := getLetterPosSetKeys(lettersFound)
+  knownPositions := set.New()
+  for _, ps := range(lettersFound) {
+    knownPositions.Add(ps.Pop()) // only one item so its to just pop once
+  }
+
+  missingLetters := set.Difference(allLettersSet, knownLetters)
+  blankPositions := set.Difference(allPositions, knownPositions)
+
+  lettersToFind := make(LetterPosSet)
+  for _, letter := range(missingLetters.List()) {
+    l, _ := letter.(string)
+    lettersToFind[l] = blankPositions.Copy()
+    // ?  cannot use blankPositions.Copy() (type set.Interface) as type *set.Set in assignment: need type assertion
+
+  }
+  return lettersToFind
+}
+
+func printGrid(grid Grid) {
+  for i := range(grid) {
+    fmt.Printf("\t\t%s\n", strings.Join(grid[i][:], " "))
+  }
 }
 
 func main() {
@@ -50,11 +115,14 @@ func main() {
   if len(os.Args) > 1 {
     puzzle = os.Args[1]
   }
-  log(fmt.Sprintf("Lets solve a gogen puzzle %s!", puzzle))
-  grid, words := extractDataFromFile(puzzle);
-  letters_found, letters_unfound,
-  log(fmt.Sprintf("%v", grid))
-  log(fmt.Sprintf("%v", words))
+  grid, lettersFound, words := extractDataFromFile(puzzle);
+  printGrid(grid)
+  log(fmt.Sprintf("Have words %v", words))
+  log(fmt.Sprintf("Have found letters %v", lettersFound))
+  log(fmt.Sprintf("Lets solve a gogen puzzle %s!\n", puzzle))
+  buildLettersToFind(lettersFound)
+  // allPositions := buildAllPositions())
+  // adjacencies = buildAdjacencies(words)
 }
 
 
