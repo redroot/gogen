ca667e8d4008b845bc3761a05e464d8fe213350d
redroot <luke@red-root.com>
2018-02-20 16:51:41 +0000

    initia l work on adding elixir

--- README.md
+++ README.md
@@ -18,3 +18,5 @@ Coming Soon: Elixir, Clojure
 - Set isn't part of the standard library so used a great third party representation. I definitely missed the unified API that Ruby provides for array/sets and maps.
 - Typed definition especially involving maps took a little while to grok, plenty of the shortcuts in Ruby unavailable but I appreciated it, if it compiles it tended to run
 - Lack of `cond || default` was a little frustrating.
+
+#### Elixir
---
+++ elixir/gogen.exs
@@ -0,0 +1,49 @@
+defmodule Position do
+  defstruct x: 0, y: 0
+end
+
+defmodule Gogen do
+  @text_input_split "#####"
+  # @grid_size 5
+  # @print_padding 3
+  @blank_character "_"
+
+  def solve(text) do
+    { grid, words, letters_found } = data_from_input(text)
+    IO.puts grid
+    IO.puts words
+    IO.puts letters_found
+  end
+
+  defp data_from_input(text) do
+    [rawGrid, rawWords] = String.split(text, @text_input_split)
+    words = String.trim(rawWords) |> String.split("\n")
+    grid =
+      String.trim(rawGrid)
+      |> String.split("\n")
+      |> Enum.map(fn x -> String.split(x, " ") end)
+      |> transpose_array
+    letters_found =
+      grid
+      |> Enum.with_index
+      |> Enum.reduce(%{}, fn({col, col_index}, letters) ->
+        Enum.each(Enum.with_index(col), fn({letter, row_index}) ->
+          unless letter == @blank_character do
+            Map.put(letters, letter, %Position{x: col_index, y: row_index})
+          end
+        end)
+        letters
+      end)
+    { grid, words, letters_found }
+  end
+
+  defp transpose_array(arr) do
+    arr
+    |> List.zip
+    |> Enum.map(&Tuple.to_list/1)
+  end
+end
+
+puzzle = System.get_env("PUZZLE") || "1"
+IO.puts "Lets solve puzzle ##{puzzle}!"
+Gogen.solve(File.read!("../examples/#{puzzle}-unsolved.txt"))
